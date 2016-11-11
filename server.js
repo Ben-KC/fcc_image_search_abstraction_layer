b076c159dd709fea76ea3350dcfd85a420326415
Benjamin Cox <benny24c@gmail.com>
2016-11-11 07:24:40 +0000

    adds basic routing

--- server.js
+++ server.js
@@ -3,4 +3,19 @@ var app = express();
 
 app.set('port', process.env.PORT || 8080);
 
+//search routing
+app.get('/api/:search', function(req, res) {
+    var search = req.params.search,
+        offset = req.query.offset || 0;
+
+    res.send(JSON.stringify({search: search, offset: offset}));
+});
+
+//recent searches
+app.get('/api/recent/searches', function(req, res) {
+    var recents = [];
+
+    res.send(recents);
+});
+
 app.listen(app.get('port'));
